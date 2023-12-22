88c95de2d6c830cb6fb4a23df3da52bffca163f5
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-22 17:19:52 +0530

    Added Common Express Middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
+import helmet from 'helmet';
+import logger from 'morgan';
 
 const port = process.env.PORT ?? 8080;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello, World</h1>');
