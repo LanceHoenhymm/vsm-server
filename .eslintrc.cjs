bcad161c8acab1db47282b3d7a2a79bcbbd9f2a3
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-22 12:48:48 +0530

    Updated eslint config
    Changed eslint config to work better with test files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,18 @@
 /* eslint-env node */
 module.exports = {
+  env: {
+    node: true,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'prettier',
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.eslint.json'],
   },
   plugins: ['@typescript-eslint'],
   root: true,
@@ -24,5 +26,13 @@ module.exports = {
     ],
     '@typescript-eslint/no-misused-promises': 'off',
   },
-  files: ['src'],
+  ignorePatterns: [
+    '.eslintrc.cjs',
+    'jest.config.cjs',
+    'e2e/jest-e2e.config.cjs',
+    '.prettierrc.cjs',
+    'node_modules',
+    'dist',
+    'old',
+  ],
 };
