6a08607bf60e4bc259fef6a6117c7a539e2d53bc
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-25 16:49:55 +0530

    Add Optional type definition

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from 'express';
 
+type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
 type ReqHandler<TReqBody, TResBody> = RequestHandler<
   object,
   TResBody,
