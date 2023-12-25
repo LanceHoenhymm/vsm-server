e91d4220da5adc18b76a53df55d4b7ceee96f4d7
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-25 17:58:13 +0530

    Add auth controller unit test

---
+++ src/controllers/auth/auth.controller.spec.ts
@@ -0,0 +1,33 @@
+import request from 'supertest';
+import express, { type Express } from 'express';
+import { registerUser } from './auth.controller';
+import { IRegisterUserDto } from './auth.controller.dto';
+
+describe('Auth Controller', () => {
+  let app: Express;
+
+  beforeAll(() => {
+    app = express();
+    app.use(express.json());
+    app.post('/register', registerUser);
+  });
+
+  describe('register endpoint', () => {
+    it('should add user data', async () => {
+      const user: IRegisterUserDto = {
+        teamId: 'Lemon',
+        email: 'abc@123.com',
+        password: 'listerine',
+        p1Name: 'Mocha',
+      };
+      const expectedResponse = {
+        status: 'Successful',
+        msg: 'Team: abc@123.com added to Database',
+      };
+      const res = await request(app).post('/register').send(user);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual(expectedResponse);
+    });
+  });
+});
