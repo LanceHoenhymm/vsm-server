7706de86eca349492cd34aab44376a2946e43d17
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-25 17:11:51 +0530

    Refactor getUniqueId function to generate a unique ID with a length of 20 characters

--- src/utils/hash.util.ts
+++ src/utils/hash.util.ts
@@ -1,7 +1,15 @@
 import argon2 from 'argon2';
 
 export function getUniqueId(baseString: string) {
-  return `${Date.now()}_${baseString}`;
+  let uid = `${baseString}${Date.now()}`;
+  if (uid.length < 20) {
+    uid += Array.from({ length: 20 - uid.length }, () =>
+      Math.floor(Math.random() * 10),
+    ).join('');
+  } else if (uid.length > 20) {
+    uid = uid.slice(0, 20);
+  }
+  return uid;
 }
 
 export function getHashedPassword(password: string) {
