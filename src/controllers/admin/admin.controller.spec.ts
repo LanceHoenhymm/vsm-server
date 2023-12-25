9108945d7e3890ea11516885b6c3a47acb2dd75f
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-25 13:32:59 +0530

    Add tests for addGameDataBatch Endpoint

--- src/controllers/admin/admin.controller.spec.ts
+++ src/controllers/admin/admin.controller.spec.ts
@@ -33,12 +33,74 @@ describe('Admin Controller', () => {
         },
       ],
     };
-
     const expectedResponse = {
       status: 'Successful',
       msg: 'GameData for round 1 was added.',
     };
     const res = await request(app).post('/addGameData').send(gameData);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(expectedResponse);
+  });
+
+  it('should handle addGameDataBatch', async () => {
+    const gameDataBatch: IGameDataBatchDto = {
+      data: [
+        {
+          roundNumber: 1,
+          news: [
+            {
+              news: 'Shetty group was getting settled in the drug market',
+              forInsider: false,
+            },
+            {
+              news: 'Rungta Group is looking for more partners to sell their weapons to',
+              forInsider: true,
+            },
+          ],
+          stocks: [
+            {
+              name: 'Chillar',
+              bpc: 0,
+            },
+            {
+              name: 'Desai',
+              bpc: -1,
+            },
+          ],
+        },
+        {
+          roundNumber: 2,
+          news: [
+            {
+              news: 'The leaders of the Desai group are caught',
+              forInsider: false,
+            },
+            {
+              news: "Majority of security organizations are buying Rungta group's weapons",
+              forInsider: true,
+            },
+          ],
+          stocks: [
+            {
+              name: 'Chillar',
+              bpc: -5,
+            },
+            {
+              name: 'Desai',
+              bpc: 2,
+            },
+          ],
+        },
+      ],
+    };
+    const expectedResponse = {
+      status: 'Successful',
+      msg: 'GameData for 2 rounds was added.',
+    };
+    const res = await request(app)
+      .post('/addGameDataBatch')
+      .send(gameDataBatch);
 
     expect(res.status).toBe(200);
     expect(res.body).toEqual(expectedResponse);
