d32b25996b19fd969f1b29ed3642943ce48e6b43
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-25 12:27:39 +0530

    Added Controller for Game Data Batch write

--- src/controllers/admin/admin.controller.ts
+++ src/controllers/admin/admin.controller.ts
@@ -1,6 +1,6 @@
+import { firestoreDB } from '../../services/db';
 import { convertedGameDataCollectionRef, GameData } from '../../model';
 import httpStatus from 'http-status-codes';
-import { BadRequest } from '../../errors';
 import type { ReqHandler, AckResponse } from '../../types';
 import type { IGameDataDto, IGameDataBatchDto } from './admin.controller.dto';
 
@@ -18,4 +18,27 @@ export const addGameData: AddGameDataHandler = async function (req, res) {
     msg: `GameData for round ${roundNumber} was added.`,
   });
 };
+
+type AddGameDataBatchHandler = ReqHandler<IGameDataBatchDto, AckResponse>;
+
+export const addGameDataBatch: AddGameDataBatchHandler = async function (
+  req,
+  res,
+) {
+  const { data } = req.body;
+  const gameDataWriteBatch = firestoreDB.batch();
+
+  for (const gameState of data) {
+    gameDataWriteBatch.set(
+      convertedGameDataCollectionRef.doc(`R${gameState.roundNumber}`),
+      new GameData(gameState.news, gameState.stocks),
+    );
+  }
+
+  await gameDataWriteBatch.commit();
+
+  res.status(httpStatus.OK).json({
+    status: 'Successful',
+    msg: `GameData for ${data.length} rounds was added.`,
+  });
 };
