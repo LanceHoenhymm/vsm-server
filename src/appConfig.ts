ea1c9ed5d8c09cddc6cfd30b65ba6cc3cf87bfbb
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-25 15:58:27 +0530

    Moved firebase config to appConfig.ts

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,4 +1,21 @@
-const gameDataCollectionName = 'game_data';
-const userCollectionName = 'users';
+import { config } from 'dotenv';
+config();
 
-export { gameDataCollectionName, userCollectionName };
+const serviceAccountConfig = {
+  projectId: process.env.PROJECT_ID,
+  clientEmail: process.env.CLIENT_EMAIL,
+  privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+};
+
+const rtDatabaseURL =
+  'https://vsm-2024-test-default-rtdb.asia-southeast1.firebasedatabase.app';
+
+const gameDataCollectionName = 'game_data_test';
+const userCollectionName = 'users_test';
+
+export {
+  gameDataCollectionName,
+  userCollectionName,
+  serviceAccountConfig,
+  rtDatabaseURL,
+};
