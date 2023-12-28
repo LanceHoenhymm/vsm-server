bf4683f766bc0df037c8ee9a33e9df523f717985
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-28 19:02:04 +0530

    Added Not Found Error

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,4 +1,5 @@
 export { ApplicationError } from './application.error';
 export { BadRequest } from './badRequest.error';
+export { NotFound } from './notFound.error';
 export { Unauthenticated } from './unauthenticated.error';
 export { Unauthorized } from './unauthorized.error';
