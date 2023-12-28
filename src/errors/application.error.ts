753491361b6642615edb4ded69293409e9cb8a58
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-28 16:35:45 +0530

    Add data parameter to ApplicationError and BadRequest classes

--- src/errors/application.error.ts
+++ src/errors/application.error.ts
@@ -2,6 +2,7 @@ export class ApplicationError extends Error {
   constructor(
     message: string,
     public statusCode: number,
+    public data?: object,
   ) {
     super(message);
   }
--- src/errors/badRequest.error.ts
+++ src/errors/badRequest.error.ts
@@ -2,7 +2,7 @@ import httpStatus from 'http-status-codes';
 import { ApplicationError } from './application.error';
 
 export class BadRequest extends ApplicationError {
-  constructor(message: string) {
-    super(message, httpStatus.BAD_REQUEST);
+  constructor(message: string, data?: object) {
+    super(message, httpStatus.BAD_REQUEST, data);
   }
 }
