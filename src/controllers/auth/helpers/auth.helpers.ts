1295070708bd0ea06ad580ebf60fc0a0f3a29eb6
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-29 10:50:46 +0530

    Refactor auth helper functions to use Firestore instead of Realtime Database

--- src/controllers/auth/helpers/auth.helpers.ts
+++ src/controllers/auth/helpers/auth.helpers.ts
@@ -1,28 +1,42 @@
 import {
-  userAccountRefName,
-  userPortfolioRefName,
-  userStatusRefName,
-  userHistoryRefName,
-} from '../../../common/appConfig';
-import { initialStartingAmount } from '../../../common/gameConfig';
-import { getRealTimeDb } from '../../../services/firebase';
+  playerDataColName,
+  playerPortColName,
+  playerStatColName,
+  playerHistColName,
+} from '../../../common/app-config';
+import { initialStartingAmount } from '../../../common/game-config';
+import {
+  PlayerDataConverter,
+  PlayerPortfolioConverter,
+  PlayerStatusConverter,
+  PlayerHistoryConverter,
+} from '../../../converters';
+import { getFirestoreDb } from '../../../services/firebase';
 
 export function setupPlayer(teamId: string) {
-  const rtDb = getRealTimeDb();
-  const userAccRef = rtDb.ref(userAccountRefName + '/' + teamId);
-  const userPortRef = rtDb.ref(userPortfolioRefName + '/' + teamId);
-  const userStatRef = rtDb.ref(userStatusRefName + '/' + teamId);
-  const userHistRef = rtDb.ref(userHistoryRefName + '/' + teamId);
+  const firestoreDb = getFirestoreDb();
+  const playerDataCollection = firestoreDb
+    .collection(playerDataColName)
+    .withConverter(PlayerDataConverter);
+  const playerPortCollection = firestoreDb
+    .collection(playerPortColName)
+    .withConverter(PlayerPortfolioConverter);
+  const playerStatCollection = firestoreDb
+    .collection(playerStatColName)
+    .withConverter(PlayerStatusConverter);
+  const playerHistoryCollection = firestoreDb
+    .collection(playerHistColName)
+    .withConverter(PlayerHistoryConverter);
 
   return Promise.all([
-    userAccRef.set({
+    playerDataCollection.doc(teamId).set({
       balance: initialStartingAmount,
       valuation: 0,
       debt: 0,
-      total: 0,
+      total: initialStartingAmount,
     }),
-    userPortRef.set({}),
-    userStatRef.set({}),
-    userHistRef.set({}),
+    playerPortCollection.doc(teamId).set({}),
+    playerStatCollection.doc(teamId).set({}),
+    playerHistoryCollection.doc(teamId).set({}),
   ]);
 }
