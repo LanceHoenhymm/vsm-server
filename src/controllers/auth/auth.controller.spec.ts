0a63d784b8f7765e02a1a9d3d5d93c34e3d21142
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-29 08:06:58 +0530

    Add Unit tests for Login Endpoint

--- src/controllers/auth/auth.controller.spec.ts
+++ src/controllers/auth/auth.controller.spec.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import express, { type Express } from 'express';
-import { registerUser } from './auth.controller';
-import { IRegisterUserDto } from './auth.controller.dto';
+import { loginUser, registerUser } from './auth.controller';
+import { ILoginUserDto, IRegisterUserDto } from './auth.controller.dto';
 
 describe('Auth Controller', () => {
   let app: Express;
@@ -10,6 +10,7 @@ describe('Auth Controller', () => {
     app = express();
     app.use(express.json());
     app.post('/register', registerUser);
+    app.post('/login', loginUser);
   });
 
   describe('register endpoint', () => {
@@ -29,6 +30,24 @@ describe('Auth Controller', () => {
       expect(res.body).toEqual(expectedRes);
     });
   });
+
+  describe('login endpoint', () => {
+    it('should run without error', async () => {
+      const req: ILoginUserDto = {
+        email: 'abc@123.com',
+        password: 'listerine',
+      };
+      const expectedRes = {
+        status: 'Successful',
+        data: {
+          token: expect.any(String) as string,
+        },
+      };
+
+      const res = await request(app).post('/login').send(req);
+
+      expect(res.status).toBe(201);
+      expect(res.body).toMatchObject(expectedRes);
     });
   });
 });
