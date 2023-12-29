a248ebbbef43d6c3c98bf8114a5e05de4055620d
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2023-12-29 07:54:51 +0530

    Bugfix: Fixed Multiple Hashes of Same password

--- src/converters/user.converter.ts
+++ src/converters/user.converter.ts
@@ -16,6 +16,7 @@ interface IUser {
 
 export class User implements IUser {
   memberCount: 1 | 2;
+  admin: boolean = false;
 
   constructor(
     public teamId: string,
@@ -23,36 +24,29 @@ export class User implements IUser {
     public password: string,
     public p1Name: string,
     public p2Name: string = '',
-    public admin: boolean = false,
   ) {
-    this.teamId = getUniqueId(teamId);
-    this.password = hashPassword(password);
     this.memberCount = p2Name ? 2 : 1;
   }
 
   verifyPassword(candidate: string) {
     return this.password === hashPassword(candidate);
   }
-
-  toPlainObject() {
-    return {
-      teamId: this.teamId,
-      email: this.email,
-      password: this.password,
-      p1Name: this.p1Name,
-      p2Name: this.p2Name,
-      memberCount: this.memberCount,
-      admin: this.admin,
-    };
-  }
 }
 
 export const userConverter = {
   toFirestore(user: User): DocumentData {
-    return user.toPlainObject();
+    return {
+      teamId: getUniqueId(user.teamId),
+      email: user.email,
+      password: hashPassword(user.password),
+      p1Name: user.p1Name,
+      p2Name: user.p2Name,
+      memberCount: user.memberCount,
+      admin: user.admin,
+    };
   },
   fromFirestore(snapshot: QueryDocumentSnapshot<IUser>): User {
-    const { teamId, email, password, p1Name, p2Name, admin } = snapshot.data();
-    return new User(teamId, email, password, p1Name, p2Name, admin);
+    const { teamId, email, password, p1Name, p2Name } = snapshot.data();
+    return new User(teamId, email, password, p1Name, p2Name);
   },
 };
