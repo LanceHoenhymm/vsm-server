2aab71545f9c37b03b75da812ee63ae3d3da33f0
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2024-03-10 16:21:52 +0530

    fead: add cache for info routes

--- src/controllers/game/info/info.router.ts
+++ src/controllers/game/info/info.router.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express';
+import { Router, type RequestHandler } from 'express';
+import { middleware } from 'apicache';
 import {
   getGameInfo,
   getNews,
@@ -8,13 +9,16 @@ import {
   getPortfolio,
   getBalence,
 } from './info.contorller.js';
+import { cacheTime } from '../../../common/app-config.js';
+
+const apiCache = middleware(cacheTime) as RequestHandler;
 
 export const infoRouter = Router();
 
-infoRouter.get('/game-info', getGameInfo);
-infoRouter.get('/news', getNews);
-infoRouter.get('/stocks', getStocks);
-infoRouter.get('/leaderboard', getLeaderboard);
+infoRouter.get('/game-info', apiCache, getGameInfo);
+infoRouter.get('/news', apiCache, getNews);
+infoRouter.get('/stocks', apiCache, getStocks);
+infoRouter.get('/leaderboard', apiCache, getLeaderboard);
 infoRouter.get('/profile', getProfile);
 infoRouter.get('/portfolio', getPortfolio);
 infoRouter.get('/balance', getBalence);
