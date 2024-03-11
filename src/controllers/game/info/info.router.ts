66a1bff03f073f949062d2065eba8679d7c8310b
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2024-03-11 20:40:32 +0530

    deps: rm apicache

--- src/controllers/game/info/info.router.ts
+++ src/controllers/game/info/info.router.ts
@@ -1,5 +1,5 @@
-import { Router, type RequestHandler } from 'express';
-import { middleware } from 'apicache';
+import { Router } from 'express';
+
 import {
   getGameInfo,
   getNews,
@@ -9,16 +9,13 @@ import {
   getPortfolio,
   getBalence,
 } from './info.contorller.js';
-import { cacheTime } from '../../../common/app-config.js';
-
-const apiCache = middleware(cacheTime) as RequestHandler;
 
 export const infoRouter = Router();
 
-infoRouter.get('/game-info', apiCache, getGameInfo);
-infoRouter.get('/news', apiCache, getNews);
-infoRouter.get('/stocks', apiCache, getStocks);
-infoRouter.get('/leaderboard', apiCache, getLeaderboard);
+infoRouter.get('/game-info', getGameInfo);
+infoRouter.get('/news', getNews);
+infoRouter.get('/stocks', getStocks);
+infoRouter.get('/leaderboard', getLeaderboard);
 infoRouter.get('/profile', getProfile);
 infoRouter.get('/portfolio', getPortfolio);
 infoRouter.get('/balance', getBalence);
