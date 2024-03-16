38ea464f431036c49c3ca3b7ada0fca0500b6cb2
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2024-03-16 12:18:33 +0530

    refactor: config update

--- src/common/app-config.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-const port = 8080;
-const allowedOrigin = '*';
-
-export { port, allowedOrigin };
---
+++ src/game/game.config.ts
@@ -0,0 +1,9 @@
+import { config } from 'dotenv';
+config();
+
+const envMaxRounds = Number(process.env.MAX_GAME_ROUNDS);
+export const maxGameRounds = isNaN(envMaxRounds) ? 0 : envMaxRounds;
+
+const envRoundDuration = Number(process.env.ROUND_DURATION);
+export const roundDuration =
+  (isNaN(envRoundDuration) ? 0 : envRoundDuration) * 60 * 1000;
