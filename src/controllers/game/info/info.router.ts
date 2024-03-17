fcccd6a903600d8f5b0aab40142393f6c0ce2d4d
Abhinav Pandey <121815498+LanceHoenhymm@users.noreply.github.com>
2024-03-17 19:04:50 +0530

    cache: hotfix

--- src/controllers/game/info/info.router.ts
+++ src/controllers/game/info/info.router.ts
@@ -8,17 +8,17 @@ import {
   getPortfolioHandler,
   getBalenceHandler,
 } from './info.contorller';
-import { cacherFactory } from '../../../middlewares/cacher.middleware';
-import { cacheTime } from '../../../common/app.config';
+// import { cacherFactory } from '../../../middlewares/cacher.middleware';
+// import { cacheTime } from '../../../common/app.config';
 
 export const infoRouter = Router();
 
-const cacher = cacherFactory(cacheTime);
+// const cacher = cacherFactory(cacheTime);
 
 infoRouter.get('/game-info', getGameInfoHandler);
-infoRouter.get('/news', cacher, getNewsHandler);
-infoRouter.get('/stocks', cacher, getStocksHandler);
-infoRouter.get('/leaderboard', cacher, getLeaderboardHandler);
+infoRouter.get('/news', getNewsHandler);
+infoRouter.get('/stocks', getStocksHandler);
+infoRouter.get('/leaderboard', getLeaderboardHandler);
 infoRouter.get('/profile', getProfileHandler);
 infoRouter.get('/portfolio', getPortfolioHandler);
 infoRouter.get('/balance', getBalenceHandler);
